4b5367d5d1355918f74c297a20767d77cea42a2e
Ernesto Alfonso <erjoalgo@gmail.com>
2018-07-28 15:21:41 -0700

    pylint

--- native/chromeurl/main.py
+++ native/chromeurl/main.py
@@ -1,79 +1,88 @@
 #!/usr/bin/python
 
+"""
+native messaging host component of chrome current url extension.
+exposes the current chrome browser url via a REST endpoint
+"""
+
 from __future__ import print_function
 from threading import Thread
 import sys
 import json
 import time
-from flask import Flask
 import argparse
 import logging
+from flask import Flask
 
 try:
     from .version import __version__
 except:
-    __version__="unknown"
-
-parser = argparse.ArgumentParser()
-parser.add_argument("extension_id") # chromium passes this positional arg on startup
-parser.add_argument("-p", "--port", default=19615)
-parser.add_argument("--log", help = "optionally log urls to a file")
-parser.add_argument("--version", action="version", version=__version__)
-parser.add_argument("-v", "--verbose", action="store_true")
-
-args=parser.parse_args()
-app = Flask(__name__)
+    __version__ = "unknown"
 
 logging.basicConfig()
 logger = logging.getLogger('chromeurl')
 
-if args.verbose:
-    logger.setLevel(logging.DEBUG)
+app = Flask(__name__)
 
-current_url=None
+current_url = None
 @app.route("/tabs/current/url")
 def get_current_url():
-    return current_url or ""
+    "echo current url"
+    return current_url or "None"
 
-def read_native_messages_loop(fh):
+def read_native_messages_loop(fh, log_fh):
+    "continuously read chrome extension messages"
     global current_url
 
-    log_fh=open(args.log, "w") if args.log else None
-
     while True:
-        msg_len_str=fh.read(4)
+        msg_len_str = fh.read(4)
         if not msg_len_str:
-            logger.debug ("got message of length 0?")
+            logger.debug("got message of length 0?")
             time.sleep(.5)
             continue
 
         # platform dependent?
-        msg_len=sum(ord(b)<<(8*(i)) for (i, b) in enumerate(msg_len_str))
+        msg_len = sum(ord(b)<<(8*(i)) for(i, b) in enumerate(msg_len_str))
 
-        logger.debug ("got message of length: {} {}".format(msg_len, (msg_len_str, len(msg_len_str))))
+        logger.debug("got message of length: %s %s", msg_len, (msg_len_str, len(msg_len_str)))
 
-        msg=fh.read(msg_len)
-        logger.debug ("message is {}".format(msg))
+        msg = fh.read(msg_len)
+        logger.debug("message is %s", msg)
 
-        data=json.loads(msg)
-        url=data["text"]
-        logger.info ("url is {}".format(url))
+        data = json.loads(msg)
+        url = data["text"]
+        logger.info("url is %s", url)
 
-        current_url=url
+        current_url = url
         if log_fh:
-            print (url, file=log_fh)
+            print(url, file=log_fh)
             log_fh.flush()
 
 def main():
+    "entry point"
     # don't write anything to stdout
-    real_stdout=sys.stdout
-    sys.stdout=sys.stderr
+    # real_stdout = sys.stdout
+    sys.stdout = sys.stderr
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("extension_id") # chromium passes this positional arg on startup
+    parser.add_argument("-p", "--port", default=19615)
+    parser.add_argument("--log", help="optionally log urls to a file")
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument("-v", "--verbose", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
     logger.info("starting native messaging stdin read loop...")
-    read_loop_thread = Thread(target = read_native_messages_loop, args = (sys.stdin, ))
+
+    log_fh = open(args.log, "w") if args.log else None
+    read_loop_thread = Thread(target=read_native_messages_loop, args=(sys.stdin, log_fh))
     read_loop_thread.start()
 
-    logger.info("starting http server on port {}".format(args.port))
+    logger.info("starting http server on port %s", args.port)
     app.run(port=args.port)
 
 
