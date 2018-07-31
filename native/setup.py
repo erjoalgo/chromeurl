4301376b0c10ac6fbe94a232418b37e12a3cab31
Ernesto Alfonso <erjoalgo@gmail.com>
2018-07-31 10:58:20 -0700

    use atexit to properly handle deps

--- native/setup.py
+++ native/setup.py
@@ -2,9 +2,8 @@ import os
 import re
 import subprocess
 import json
-from setuptools.command.develop import develop
 from setuptools.command.install import install
-
+import atexit
 from setuptools import setup
 
 # Utility function to read the README file.
@@ -19,43 +18,47 @@ VERSION = re.search("[0-9.]+", read("chromeurl/version.py")).group(0)
 EXE = "chromeurl"
 PACKAGE = "chromeurl"
 
-class PostInstallCommand(install):
+def install_manifest():
     """install chrome native host manifest file"""
-    def run(self):
-        # first install the native host
-        install.run(self)
-        EXE_ABS = subprocess.check_output(["which", EXE]).strip()
-        # then install the manifest at the appropriate location
+    EXE_ABS = subprocess.check_output(["which", EXE]).strip()
 
-        for target_parent in ["~/Library/Application Support/Google/", "~/.config/"]:
-            expanded=os.path.expanduser(target_parent)
-            dirnames=os.listdir(expanded) if os.path.exists(expanded) else []
-            cands=[dirname for dirname in dirnames if re.search("chrom", dirname)]
-            if cands:
-                target=os.path.join(target_parent, cands[0], "NativeMessagingHosts")
-                break
-        else:
-            print ("cannot discover chrome config directory")
-            exit(1)
+    for target_parent in ["~/Library/Application Support/Google/", "~/.config/"]:
+        expanded=os.path.expanduser(target_parent)
+        dirnames=os.listdir(expanded) if os.path.exists(expanded) else []
+        cands=[dirname for dirname in dirnames if re.search("chrom", dirname)]
+        if cands:
+            target=os.path.join(target_parent, cands[0], "NativeMessagingHosts")
+            break
+    else:
+        print ("cannot discover chrome config directory")
+        exit(1)
 
-        host_name = "com.erjoalgo.chrome_current_url"
-        manifest_path = os.path.expanduser(os.path.join(target, "{}.json".format(host_name)))
-        extension_id = "ekofhnkbloagjhkldkgjcbmfealemjnh"
+    host_name = "com.erjoalgo.chrome_current_url"
+    manifest_path = os.path.expanduser(os.path.join(target, "{}.json".format(host_name)))
+    extension_id = "ekofhnkbloagjhkldkgjcbmfealemjnh"
 
-        manifest = {
-            "name": host_name,
-            "description": "chrome current url native host component",
-            "path": EXE_ABS,
-            "type": "stdio",
-            "allowed_origins": [
-                "chrome-extension://{}/".format(extension_id)
-            ]
-        }
+    manifest = {
+        "name": host_name,
+        "description": "chrome current url native host component",
+        "path": EXE_ABS,
+        "type": "stdio",
+        "allowed_origins": [
+            "chrome-extension://{}/".format(extension_id)
+        ]
+    }
 
-        with open(manifest_path, "w") as fh:
-            json.dump(manifest, fh, indent=4)
+    with open(manifest_path, "w") as fh:
+        json.dump(manifest, fh, indent=4)
 
-        print ("Native messaging host {} has been installed at {}".format(host_name, manifest_path))
+    print ("Native messaging host {} has been installed at {}".format(host_name, manifest_path))
+
+_post_install=install_manifest
+
+class new_install(install, object):
+    def __init__(self, *args, **kwargs):
+        super(new_install, self).__init__(*args, **kwargs)
+        atexit.register(_post_install)
+
 
 setup(
     name="chromeurl",
@@ -81,9 +84,8 @@ setup(
         ]
     },
     cmdclass={
-        'install': PostInstallCommand,
+        'install': new_install,
     },
-
 )
 
 
