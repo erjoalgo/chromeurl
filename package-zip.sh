430c8faea265074bf3b7637f641bfb97d207fc82
Ernesto Alfonso <erjoalgo@gmail.com>
2020-07-06 01:57:41 -0700

    cd to SELFD and don't fail on rm

--- package-zip.sh
+++ package-zip.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-rm chrome-current-url*.zip
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+rm -f chrome-current-url*.zip
 ZIP=chrome-current-url-$(date -I).zip
 cd chrome
 zip -r ${ZIP} *
